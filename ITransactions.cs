54464170a84dd448d3421079088a735a40b002d6
Salihler <eren.salihler@gmail.com>
2022-04-27 00:19:46 +0300

    improvements

---
+++ ITransactions.cs
@@ -0,0 +1,12 @@
+using System.Management;
+
+namespace ChangeNetwork
+{
+    public interface ITransactions
+    {
+        public List<ManagementObject> GetAdapters();
+        public ManagementObject GetSelectedAdapter(List<ManagementObject> networks);
+        public int GetProcess();
+        public string Change(ManagementObject changedElement, int process);
+    }
+}
